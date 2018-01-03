a233ddcdedadd5d2b1dbdac3584303c1c1f699c5
lvwanyou <lvwanyou@163.com>
2018-01-03 17:27:17 +0800

    FSM添加
    FSM添加

--- test7.py
+++ test7.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from datetime  import date
+from test_thread import print_time
 now=date.today()
 now.strftime("%y/%m/%d")
+print_time()
 print(now)
 birthday=date(1995,9,15)
 print((now-birthday).days)
--- test_thread.py
+++ test_thread.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-import _thread
 import time
+import _thread
 
 
 # 为线程定义一个函数
@@ -17,7 +16,6 @@ def print_time(threadName, delay):
 try:
     _thread.start_new_thread(print_time, ("Thread-1", 2,))
     _thread.start_new_thread(print_time, ("Thread-2", 4,))
-    print(_thread.ne)
 except:
     print("Error: 无法启动线程")
 
